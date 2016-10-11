b6b9f49433b4309dd002a3b029cbfe6a54394a52
Alex0319 <alexandrsavchuk.97@gmail.com>
2016-10-12 00:36:35 +0300

    Add class for create threads

--- ConcurrentLogger/LoggerTargetWriteToFile.cs
+++ ConcurrentLogger/LoggerTargetWriteToFile.cs
@@ -12,23 +12,26 @@ namespace ConcurrentLogger
         public bool Flush(LogInfo logInfo)
         {
             Task<bool> t= FlushAsync(logInfo);
-            return t.Result;
+            return t.IsFaulted;
         }
 
         public Task<bool> FlushAsync(LogInfo logInfo)
         {
             return Task.Run<bool>(()=> {
+                bool result = false;
                 StreamWriter streamWriter = new StreamWriter("FileLog.txt", true);    
                 try
                 {
-                    streamWriter.WriteLine(logInfo.level+" "+logInfo.message);
+                    
+                    streamWriter.WriteLine("[ "+logInfo.time+" ] "+logInfo.level+" "+logInfo.message);
+                    result = true;
+                    return result;
                 }
                 finally
                 {
                     streamWriter.Close();
                     streamWriter.Dispose();
                 }
-                return true;
             });         
         }
     }
